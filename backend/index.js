5b8293d7e72dede319939df5ea12523a42596ccc
Zayyan Zameeth <101924356+zayyanzameeth@users.noreply.github.com>
2023-03-15 09:48:08 +0530

    Backend Changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
-import server from "./server.js";
+const server  = require ("./server.js");
 
-server.listen(8080, function () {
-  console.log(
-    "Capstone Project Backend is running on http://localhost:8080"
-  );
-});
+if(process.env.NODE_ENV === "test"){
+  server.listen(3000, () => {
+    console.log(
+      "Capstone Project Backend is running on http://localhost:3000"
+    );
+  });
+}else{
+  server.listen(8080, () => {
+    console.log(
+      "Capstone Project Backend is running on http://localhost:8080"
+    );
+  });
+}
